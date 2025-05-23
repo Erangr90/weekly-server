d7846b35a441da3b5ca66d343b1fee53f4434d28
Erangr90 <erangrady3.0@gmail.com>
2025-05-23 17:34:39 +0300

    deploy to EBT of AWS

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,12 @@
 module.exports = {
-    apps: [
-        {
-            name: "server",
-            script: "dist/index.js",
-            instances: 1, // or 'max' for cluster mode if desired
-            exec_mode: "fork", // or 'cluster' if you're comfortable managing it
-            env: {
-                NODE_ENV: "production",
-            },
-        },
-    ],
+    apps: [{
+        name: "server",
+        script: "dist/index.js",
+        instances: 1,
+        exec_mode: "fork",
+        env: {
+            NODE_ENV: "production",
+            PORT: process.env.PORT
+        }
+    }]
 };
